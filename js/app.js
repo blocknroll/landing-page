e3697555be71c83771eb50618551166021e412a3
Fred Block <info@fredblock.com>
2020-04-22 18:41:58 -0500

    feat: Add initial navbar elements

--- js/app.js
+++ js/app.js
@@ -1,36 +1,79 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
+
+
+
+
+
+
+
+
+
+
+// NAV <ul>
+const navbarList = document.getElementById("navbar__list");
+
+// select all sections
+const sections = document.getElementsByTagName("section");
+
+// loop through sections - create li's
+for (const section of sections) {
+  // create li
+  const newLi = document.createElement("li");
+  // grab text of section.id
+  const textOfLi = document.createTextNode(section.id);
+  // add section.id to li
+  newLi.appendChild(textOfLi);
+  // add newLi to nav ul
+  navbarList.appendChild(newLi);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /**
  * End Global Variables
+
+
  * Start Helper Functions
- * 
+ *
 */
 
 
 
 /**
  * End Helper Functions
+
+
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
@@ -45,13 +88,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
